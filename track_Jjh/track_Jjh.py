97298d2237329355390fa942ce9bd84c60619394
taesla <cjstkdus6978@gmail.com>
2021-08-17 13:24:29 +0900

    test

--- track_Jjh/track_Jjh.py
+++ track_Jjh/track_Jjh.py
@@ -25,8 +25,13 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Point , PoseStamped
 from ackermann_msgs.msg import AckermannDriveStamped
 from cv_bridge import CvBridge, CvBridgeError
+
+BoundingBox = []
+image_shape = []
+
+
 def find_delta(image_shape, going_pixels):
-    123
+    
     ############################# angle1 ###################################
     #print(going_pixels[0]-(image_shape[1]/2))
     angle_radian = math.atan(((going_pixels[0]-(image_shape[1]/2))/2)/(image_shape[0]-going_pixels[1])) ## /2
@@ -68,45 +73,68 @@ def image_callback(data):
     global image_shape
     try: # Read the image from the input topic:
         cv_image = bridge.imgmsg_to_cv2(data)
-    except CvBridgeError, e:
-            print e
+    except CvBridgeError as e:
+        print(e)
+    kernel = np.ones((3, 3), np.uint8)
+    cv_image = cv2.erode(cv_image, kernel, iterations=1)
+    cv_image = cv2.dilate(cv_image, kernel, iterations=1)
     frame = np.zeros((cv_image.shape[0],cv_image.shape[1],3),np.uint8)
-    frame[:,:,0] = cv_image
-    frame[:,:,1] = cv_image
-    frame[:,:,2] = cv_image
+    frame[:,:] = cv_image
     image_shape=[frame.shape[0],frame.shape[1]]
     
 
 def BoundingBoxes_callback(data):
     global BoundingBox
-    cone_xmin = data.bounding_boxes[0].xmin
-    cone_ymin = data.bounding_boxes[0].ymin #ymin
-    cone_xmax = data.bounding_boxes[0].xmax #xmax
-    cone_ymax = data.bounding_boxes[0].ymax #ymax
-    cone_class = data.bounding_boxes[0].Class
-    cone_y_center = (cone_ymax+cone_ymin)/2
-    cone_x_center = (cone_xmax+cone_xmin)/2
-    cone_box_size = cal_boxsize(cone_xmin,cone_ymin,cone_xmax,cone_ymax)
-    BoundingBox =  [cone_xmin,cone_ymin,cone_xmax,cone_ymax,cone_y_center,cone_x_center,cone_box_size,cone_class]
+    count = 0
+    #print(data)
+    for i in range(len(data.bounding_boxes)):
+        cone_xmin = data.bounding_boxes[i].xmin
+        cone_ymin = data.bounding_boxes[i].ymin #ymin
+        cone_xmax = data.bounding_boxes[i].xmax #xmax
+        cone_ymax = data.bounding_boxes[i].ymax #ymax
+        cone_class = data.bounding_boxes[i].Class
+        cone_y_center = (cone_ymax+cone_ymin)/2
+        cone_x_center = (cone_xmax+cone_xmin)/2
+        cone_box_size = cal_boxsize(cone_xmin,cone_ymin,cone_xmax,cone_ymax)
+        BoundingBox.append([cone_xmin,cone_ymin,cone_xmax,cone_ymax,cone_y_center,cone_x_center,cone_box_size,cone_class])
+    
+    
+    
+
+    #print(BoundingBox)
 
 def Cone_information(data):
+    #print(data)
     Blue_informations = []
     Yello_informations = []
     Blue_information = []
     Yello_information = []
-    Class=data[7]
-    if (Class=='blue'):
-        blue_cone_xmin,blue_cone_ymin,blue_cone_xmax,blue_cone_ymax,blue_cone_y_center,blue_cone_x_center,blue_cone_box_size = data[0:7]
-        Blue_information = [blue_cone_xmin,blue_cone_ymin,blue_cone_xmax,blue_cone_ymax,blue_cone_y_center,blue_cone_x_center,blue_cone_box_size]
-        Blue_informations.append(Blue_information)
-    elif (Class=='yellow'):
-       [yello_cone_xmin,yello_cone_ymin,yello_cone_xmax,yello_cone_ymax,yello_cone_y_center,yello_cone_x_center,yello_cone_box_size] = data[0:7]
-       Yello_information = [yello_cone_xmin,yello_cone_ymin,yello_cone_xmax,yello_cone_ymax,yello_cone_y_center,yello_cone_x_center,yello_cone_box_size]
-       Yello_informations.append(Yello_information)
-    print(Blue_informations,Yello_informations)
-    return [Blue_informations,Yello_informations]
+        for i in range(len(data)):
+            Class=data[i][7]
+            #print(Class)
+            if (Class =='blue'):
+                [blue_cone_xmin,blue_cone_ymin,blue_cone_xmax,blue_cone_ymax,blue_cone_y_center,blue_cone_x_center,blue_cone_box_size] = data[i][0:7]
+                Blue_information = [blue_cone_xmin,blue_cone_ymin,blue_cone_xmax,blue_cone_ymax,blue_cone_y_center,blue_cone_x_center,blue_cone_box_size]
+                #print(Blue_information)
+                Blue_informations.append(Blue_information)
+                #print(Blue_informations)
+                
+            elif (Class=='yellow'):
+                [yello_cone_xmin,yello_cone_ymin,yello_cone_xmax,yello_cone_ymax,yello_cone_y_center,yello_cone_x_center,yello_cone_box_size] = data[i][0:7]
+                Yello_information = [yello_cone_xmin,yello_cone_ymin,yello_cone_xmax,yello_cone_ymax,yello_cone_y_center,yello_cone_x_center,yello_cone_box_size]
+                #print(Yello_information)
+                Yello_informations.append(Yello_information)
+                #print(Yello_informations)
+            if(i ==len(data)-1 ):
+                print(Blue_informations,Yello_informations)
+                
+                return [Blue_informations,Yello_informations]
+    
+    del BoundingBox [:]
+    
 
 def Select_biggest_box(data):
+    #print(data)
     box_size_max = np.array(data)[:,6].max() #6 is boxsize collunm
     box_size_max_num = np.wehre(np.array(data)[:,6]==box_size_max)
     biggest_box_information = np.array(data)[box_size_max_num,:]
@@ -119,19 +147,19 @@ def Make_pixel(box1_data,box2_data): #make target coordinates
     return going_pixels
 
 if __name__ == '__main__':
-    
     rospy.init_node('Track_mission', anonymous=True)
     bridge = CvBridge()
-    rospy.Subscriber("darknet_ros/detection_image", Image, image_callback)
-    rospy.Subscriber("darknet_ros/bounding_boxes", BoundingBoxes, BoundingBoxes_callback)
+    rospy.Subscriber("/darknet_ros/detection_image", Image, image_callback)
+    rospy.Subscriber("/darknet_ros/bounding_boxes", BoundingBoxes, BoundingBoxes_callback)
     image_publisher = rospy.Publisher("/final_image", Image, queue_size=100)
     control_publisher = rospy.Publisher("/Lane_ack_vel", AckermannDriveStamped, queue_size=100)
     rate = rospy.Rate(10)
     ackermann_cmd = AckermannDriveStamped()
     while (True):
         try:
-            [Blue_informations,Yello_informations] = Cone_information(BoundingBox)
-            Blue_biggest_box = Select_biggest_box(Blue_informations)
+            [Blue_informations,Yello_informations]= Cone_information(BoundingBox)
+            print(Blue_informations,Yello_informations)
+            '''Blue_biggest_box = Select_biggest_box(Blue_informations)
             Yello_biggest_box = Select_biggest_box(Yello_informations) #make_point
             going_pixels = Make_pixel(Blue_biggest_box,Yello_biggest_box)
             final_image, image_delta = find_delta(image_shape,going_pixels) #make_delta
@@ -150,7 +178,7 @@ if __name__ == '__main__':
             control_publisher.publish(ackermann_cmd) #publish
             image_publisher.publish(bridge.cv2_to_imgmsg(final_image)) #publish
             if cv2.waitKey(1) == ord('q'):
-                break
+                break'''
 
         except rospy.ROSInterruptException:
             pass
